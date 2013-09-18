f2243071dab4a202832b493047ad0b0b0aff05f0
ruuts <ruud@seydel.me>
2013-09-18 15:52:37 +0200

    Make cms-admin available under :locale/
    https://trello.com/c/rjnBdXmS

--- lib/comfortable_mexican_sofa/routing.rb
+++ lib/comfortable_mexican_sofa/routing.rb
@@ -4,34 +4,36 @@ module ComfortableMexicanSofa::Routing
     options[:path] ||= 'cms-admin'
     
     Rails.application.routes.draw do
-      namespace :cms_admin, :path => options[:path], :except => :show do
-        get '/', :to => 'base#jump'
-        resources :sites do
-          resources :pages do
-            get  :form_blocks,    :on => :member
-            get  :toggle_branch,  :on => :member
-            put :reorder,         :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              put :revert, :on => :member
+      scope "(/:locale)", :locale => /nl|de|en/ do
+        namespace :cms_admin, :path => options[:path], :except => :show do
+          get '/', :to => 'base#jump'
+          resources :sites do
+            resources :pages do
+              get  :form_blocks,    :on => :member
+              get  :toggle_branch,  :on => :member
+              put :reorder,         :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                put :revert, :on => :member
+              end
             end
-          end
-          resources :files do
-            put :reorder, :on => :collection
-          end
-          resources :layouts do
-            put :reorder, :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              put :revert, :on => :member
+            resources :files do
+              put :reorder, :on => :collection
             end
-          end
-          resources :snippets do
-            put :reorder, :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              put :revert, :on => :member
+            resources :layouts do
+              put :reorder, :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                put :revert, :on => :member
+              end
             end
+            resources :snippets do
+              put :reorder, :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                put :revert, :on => :member
+              end
+            end
+            resources :categories
+            get 'dialog/:type' => 'dialogs#show', :as => 'dialog'
           end
-          resources :categories
-          get 'dialog/:type' => 'dialogs#show', :as => 'dialog'
         end
       end
     end
